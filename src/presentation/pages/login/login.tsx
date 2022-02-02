dedee1816ef1f965e0ae2ecb9a45fa94e6e83814
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-02 16:13:18 -0300

    refactor: move input to it's own file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,6 +3,7 @@ import Styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
 import LoginHeader from '@/presentation/components/login-header/login-header'
 import Footer from '@/presentation/components/footer/footer'
+import Input from '@/presentation/components/input/input'
 
 const Login: React.FC = () => {
 
@@ -11,14 +12,9 @@ const Login: React.FC = () => {
             <LoginHeader></LoginHeader>
             <form className={Styles.form}>
                 <h2> Login </h2>
-                <div className={Styles.inputWrap}>
-                    <input type="email" name="email" placeholder='Digite seu e-mail' ></input>
-                    <span className={Styles.status}>👌</span>
-                </div>
-                <div className={Styles.inputWrap}>
-                    <input type="password" name="senha" placeholder='Digite sua senha' ></input>
-                    <span className={Styles.status}>👌</span>
-                </div>
+                <Input type="email" name="email" placeholder='Digite seu e-mail'></Input>
+                <Input type="password" name="senha" placeholder='Digite sua senha'></Input>
+
                 <button className={Styles.submit} type="submit">Entrar</button>
                 <span className={Styles.link}>Criar conta</span>
                 <div className={Styles.errorWrap}>
