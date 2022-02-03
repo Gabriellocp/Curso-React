34af21ecc82e5141834305d755aa4201fb89c8f5
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-03 18:26:17 -0300

    feat: ensure login start with required field

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import Styles from "./input-styles.scss"
-
+import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 const Input: React.FC<Props> = (props: Props) => {
     const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
         event.target.readOnly = false
     }
+    const { errorState } = useContext(Context)
+    const error = errorState[props.name]
+    const getStatus = (): string => {
+        return '👌'
+    }
+    const getTitle = (): string => {
+        return error
+    }
     return (
         <div className={Styles.inputWrap}>
             <input {...props} readOnly onFocus={enableInput}></input>
-            <span className={Styles.status}>👌</span>
+            <span data-testid={`${props.name}Status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
         </div>
     )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -2,27 +2,28 @@ import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
-type StateProps = {
-    isLoading: boolean
-    errorMessage: string
-    disabled: boolean
-}
+
 const Login: React.FC = () => {
-    const [state] = useState<StateProps>({
+    const [state] = useState({
         isLoading: false,
-        errorMessage: '',
-        disabled: true
+
+
+    })
+    const [errorState] = useState({
+        main: '',
+        email: 'Campo obrigatório',
+        password: 'Campo obrigatório'
     })
 
     return (
         <div className={Styles.login}>
             <LoginHeader></LoginHeader>
 
-            <Context.Provider value={state}>
+            <Context.Provider value={{ state, errorState }}>
                 <form className={Styles.form}>
                     <h2> Login </h2>
                     <Input type="email" name="email" placeholder='Digite seu e-mail'></Input>
-                    <Input type="password" name="senha" placeholder='Digite sua senha'></Input>
+                    <Input type="password" name="password" placeholder='Digite sua senha'></Input>
                     <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
                     <span className={Styles.link}>Criar conta</span>
                     <FormStatus></FormStatus>
