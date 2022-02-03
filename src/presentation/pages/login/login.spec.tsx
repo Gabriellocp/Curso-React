de7dacf1541c0f2563741bda2195180baf595304
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-03 18:08:54 -0300

    refactor: moved all tests to unique one

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import { getByTestId, render } from '@testing-library/react'
 import Login from './login'
 describe('Login Component', () => {
-    test('Loading and error components can\'t be rendered at screen\'s creation', () => {
+    test('Should start with initial components disabled', () => {
         const { getByTestId } = render(<Login></Login>)
         const errorWrap = getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
-    });
-    test('Enter button not rendered at screen\'s creation', () => {
-        const { getByTestId } = render(<Login></Login>)
         const button = getByTestId('submit') as HTMLButtonElement
         expect(button.disabled).toBe(true)
     });
