ffbd397a3a05a07d11386ec35d77415aa0e40e4e
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-04 16:01:33 -0300

    refactor: create factory for login.spec.ts test

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { getByTestId, render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+    sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+    const sut = render(<Login></Login>)
+    return {
+        sut
+    }
+}
 describe('Login Component', () => {
     test('Should start with initial components disabled', () => {
-        const { getByTestId } = render(<Login></Login>)
-        const errorWrap = getByTestId('error-wrap')
+        const { sut } = makeSut()
+        const errorWrap = sut.getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
-        const button = getByTestId('submit') as HTMLButtonElement
+        const button = sut.getByTestId('submit') as HTMLButtonElement
         expect(button.disabled).toBe(true)
-        const emailStatus = getByTestId('emailStatus')
+        const emailStatus = sut.getByTestId('emailStatus')
         expect(emailStatus.title).toBe('Campo obrigatório')
         expect(emailStatus.textContent).toBe('👌')
-        const passwordStatus = getByTestId('passwordStatus')
+        const passwordStatus = sut.getByTestId('passwordStatus')
         expect(passwordStatus.title).toBe('Campo obrigatório')
         expect(passwordStatus.textContent).toBe('👌')
 
