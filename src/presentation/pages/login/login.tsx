0dda0f43ddff141d0d216957a7e0b43e7a4e60b1
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-08 14:38:19 -0300

    feat: ensure Login shows spinner on submit click

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -33,18 +33,23 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
         }));
     }, [state.password, validation]);
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault()
+        setState({ ...state, isLoading: true })
+    }
+
     return (
         <div className={Styles.login}>
             <LoginHeader></LoginHeader>
 
             <Context.Provider value={{ state, setState }}>
-                <form className={Styles.form}>
+                <form className={Styles.form} onSubmit={handleSubmit}>
                     <h2> Login </h2>
                     <Input type="email" name="email" placeholder='Digite seu e-mail'></Input>
                     <Input type="password" name="password" placeholder='Digite sua senha'></Input>
                     <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type="submit">Entrar</button>
                     <span className={Styles.link}>Criar conta</span>
-                    <FormStatus></FormStatus>
+                    <FormStatus ></FormStatus>
                 </form>
             </Context.Provider>
 
