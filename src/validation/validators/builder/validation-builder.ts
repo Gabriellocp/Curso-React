5c466a0a0a4bc3180ddff050c5eed76ed19cfbfe
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-09 17:52:47 -0300

    feat: ensure ValidationBuilder returns an instance of MinLengthValidation

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,5 +1,5 @@
 import { FieldValidation } from "@/validation/protocols/field-validation"
-import { RequiredFieldValidation, EmailValidation } from "@/validation/validators"
+import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from "@/validation/validators"
 
 export class ValidationBuilder {
     private constructor(
@@ -23,6 +23,10 @@ export class ValidationBuilder {
         return this
     }
 
+    min(length: number): ValidationBuilder {
+        this.validations.push(new MinLengthValidation(this.fieldName, length))
+        return this
+    }
     build(): FieldValidation[] {
         return this.validations
     }
