504a8f269789e3118a02a6acdd3c4a81d8db2301
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-09 15:39:47 -0300

    test: ensure email validation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,17 @@
+import { InvalidFieldErrror } from "@/validation/errors/invalid-field-error"
+import { FieldValidation } from "@/validation/protocols/field-validation"
+import { EmailValidation } from "./email-validation"
+
+
+
+
+describe('EmailValidation', () => {
+    test('Should return error if email is invalid', () => {
+        const sut = new EmailValidation('email')
+        const error = sut.validate('')
+        expect(error).toEqual(new InvalidFieldErrror('email'))
+
+    })
+
+
+})
