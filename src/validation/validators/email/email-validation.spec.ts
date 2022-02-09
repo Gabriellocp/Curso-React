d2f6a2e55fd27d5864d8bf0eb5c45699952b339f
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-09 15:46:42 -0300

    refactor: create factory to use within SUT

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,18 +2,18 @@ import { InvalidFieldErrror } from "@/validation/errors/invalid-field-error"
 import { EmailValidation } from "./email-validation"
 import faker from 'faker'
 
-
+const makeSut = (field: string): EmailValidation => new EmailValidation(field)
 
 describe('EmailValidation', () => {
     test('Should return error if email is invalid', () => {
         const field = faker.random.word()
-        const sut = new EmailValidation(field)
+        const sut = makeSut(field)
         const error = sut.validate(faker.random.word())
         expect(error).toEqual(new InvalidFieldErrror(field))
 
     })
     test('Should return false if email is valid', () => {
-        const sut = new EmailValidation(faker.random.word())
+        const sut = makeSut(faker.random.word())
         const error = sut.validate(faker.internet.email())
         expect(error).toBeFalsy()
 
