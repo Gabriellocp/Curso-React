2b48790e8d16b526ae6dfc51d37d9f8251edc61d
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-09 15:22:52 -0300

    test: requiredFieldValidation return false if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,6 +1,6 @@
 import { RequiredFieldError } from "../errors"
 import { RequiredFieldValidation } from "./required-field-validation"
-
+import faker from 'faker'
 
 
 
@@ -10,5 +10,10 @@ describe('RequiredFieldValidation', () => {
         const sut = new RequiredFieldValidation('email')
         const error = sut.validate('')
         expect(error).toEqual(new RequiredFieldError())
+    })
+    test('Should return false if field is not empty', () => {
+        const sut = new RequiredFieldValidation('email')
+        const error = sut.validate(faker.random.word())
+        expect(error).toBeFalsy()
     })
 })
