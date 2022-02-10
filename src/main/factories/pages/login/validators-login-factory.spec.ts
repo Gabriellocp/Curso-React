323ef223de3375b19f306e98e4add0f1f245d0dd
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-10 19:02:31 -0300

    test: ensure ValidationComposite uses correct validators

---
+++ src/main/factories/pages/login/validators-login-factory.spec.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite } from "@/validation/validators"
+import { ValidationBuilder } from "@/validation/validators/builder/validation-builder"
+import { makeLoginValidators } from "./validators-login-factory"
+
+describe('LoginValidationFactory', () => {
+    test('Should compose ValidationComposite with correct validations', () => {
+        const composite = makeLoginValidators()
+        expect(composite).toEqual(ValidationComposite.build(
+            [
+                ...ValidationBuilder.field('email').required().email().build(),
+                ...ValidationBuilder.field('password').required().min(5).build(),
+
+            ]
+        ))
+    })
+})
