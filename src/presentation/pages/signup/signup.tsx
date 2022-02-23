a84bd37b240189d5bf24b574bbb30bc4471c081b
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-23 16:36:37 -0300

    feat: ensure SignUp screen starts with initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './signup-styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
-import { Link } from 'react-router-dom'
 
 const Signup: React.FC = () => {
+    const [state] = useState({
+        isLoading: false,
+        mainError: '',
+        emailError: 'Campo obrigatório',
+        passwordError: 'Campo obrigatório',
+        passwordConfirmationError: 'Campo obrigatório',
+        nameError: 'Campo obrigatório'
+    })
 
     return (
-        <div className={Styles.login}>
+        <div className={Styles.signup}>
             <LoginHeader></LoginHeader>
 
-            <Context.Provider value={{ state: {} }}>
+            <Context.Provider value={{ state }}>
                 <form data-testid='form' className={Styles.form} >
                     <h2> Signup </h2>
-                    <Input type="text" name="email" placeholder='Digite seu nome'></Input>
+                    <Input type="text" name="name" placeholder='Digite seu nome'></Input>
                     <Input type="email" name="email" placeholder='Digite seu e-mail'></Input>
-                    <Input type="password" name="email" placeholder='Digite sua senha'></Input>
-                    <Input type="password" name="password" placeholder='Confirme sua senha'></Input>
-                    <button className={Styles.submit} type="submit">Entrar</button>
-                    <Link to="/login" className={Styles.link}>Voltar para Login</Link>
+                    <Input type="password" name="password" placeholder='Digite sua senha'></Input>
+                    <Input type="passwordConfirmation" name="passwordConfirmation" placeholder='Confirme sua senha'></Input>
+                    <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
+                    <span className={Styles.link}>Voltar para Login</span>
                     <FormStatus ></FormStatus>
                 </form>
             </Context.Provider>
