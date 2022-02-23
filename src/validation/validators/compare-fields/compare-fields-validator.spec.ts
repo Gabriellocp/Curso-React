d63d7a4934e920e99793881d323e0ca4b3aa6e11
Gabriellocp <gabriel_campista@hotmail.com>
2022-02-23 16:01:07 -0300

    test: ensure CompareFieldValidation returns falsy if compare is valid

--- src/validation/validators/compare-fields/compare-fields-validator.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validator.spec.ts
@@ -10,4 +10,11 @@ describe('CompareFieldValidation', () => {
         const error = sut.validate(faker.random.word())
         expect(error).toEqual(new InvalidFieldError(field))
     })
+    test('Should return falsy if compare is valid', () => {
+        const field = faker.database.column()
+        const valueToCompare = faker.random.word()
+        const sut = makeSut(field, valueToCompare)
+        const error = sut.validate(valueToCompare)
+        expect(error).toBeFalsy()
+    })
 })
