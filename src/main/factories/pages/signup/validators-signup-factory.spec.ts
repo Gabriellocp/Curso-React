4d672cc2966ff8bc5370d8be4d257db92d72f28a
Gabriellocp <gabriel_campista@hotmail.com>
2022-03-07 17:20:25 -0300

    test: add SignUp validation factory

---
+++ src/main/factories/pages/signup/validators-signup-factory.spec.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite } from "@/validation/validators"
+import { ValidationBuilder } from "@/validation/validators/builder/validation-builder"
+import { makeSignUpValidators } from "./validators-signup-factory"
+
+describe('SignUpValidationFactory', () => {
+    test('Should compose ValidationComposite with correct validations', () => {
+        const composite = makeSignUpValidators()
+        expect(composite).toEqual(ValidationComposite.build(
+            [...ValidationBuilder.field('name').required().build(),
+            ...ValidationBuilder.field('email').required().email().build(),
+            ...ValidationBuilder.field('password').required().min(5).build(),
+            ...ValidationBuilder.field('passwordConfirmation').required().min(5).build()
+            ]
+        ))
+    })
+})
