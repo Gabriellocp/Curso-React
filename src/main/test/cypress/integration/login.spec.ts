60711680d7ac9b800a8cd39dc32a2e7e035e4ff4
Gabriellocp <gabriel_campista@hotmail.com>
2022-03-10 17:38:50 -0300

    test: ensure login save AccessToken if valid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -50,5 +50,16 @@ describe('Login', () => {
             .getByTestId('mainError').should('contain.text', 'Credenciais inválidas')
         cy.url().should('eq', `${baseUrl}/login`)
     })
+    it('Should present save AccessToken if valid credentials are provided', () => {
+        cy.getByTestId('email').focus().type('mango@gmail.com')
+        cy.getByTestId('password').focus().type('12345')
+        cy.getByTestId('submit').click()
+        cy.getByTestId('error-wrap')
+            .getByTestId('spinner').should('exist')
+            .getByTestId('mainError').should('not.exist')
+            .getByTestId('spinner').should('not.exist')
+        cy.url().should('eq', `${baseUrl}/`)
+        cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+    })
 
 })
