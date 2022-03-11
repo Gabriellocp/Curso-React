0fcf49436956de09250d20dbf007768fefceb7ef
Gabriellocp <gabriel_campista@hotmail.com>
2022-03-11 17:10:15 -0300

    test: ensure saveAccessToken throws error if undefined accessToken is provided

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,6 +1,7 @@
 import { SetStorageMock } from '@/data/test'
 import { LocalSaveAccessToken } from './local-save-access-token'
 import faker from 'faker'
+import { UnexpectedError } from '@/domain/errors'
 
 type SutTypes = {
     sut: LocalSaveAccessToken
@@ -30,5 +31,10 @@ describe('LocalSaveAccessToken', () => {
         jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
         const promise = sut.save(faker.random.uuid())
         await expect(promise).rejects.toThrow(new Error())
+    })
+    test('Should throw accessToken is falsy', async () => {
+        const { sut, setStorageMock } = makeSut()
+        const promise = sut.save(undefined)
+        await expect(promise).rejects.toThrow(new UnexpectedError())
     })
 })
