69a01f013df3abbca2ba75fbd01c5592d2095f5a
Gabriellocp <gabriel_campista@hotmail.com>
2022-03-21 17:20:48 -0300

    test: ensure RemoteLoadSrveyList calls HttpGetClient with correct url

---
+++ src/data/test/mock-http-get.ts
@@ -0,0 +1,8 @@
+import { HttpGetClient, HttpGetParams } from "../protocols/http"
+
+export class HttpGetClientSpy implements HttpGetClient {
+    url: string
+    async get(params: HttpGetParams): Promise<void> {
+        this.url = params.url
+    }
+}
