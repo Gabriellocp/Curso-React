53e29b58719a2d2da2e6a8063f264b224f0c38f3
Gabriellocp <gabriel_campista@hotmail.com>
2022-03-25 16:10:28 -0300

    feat: add survey iconText to show status

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -19,6 +19,10 @@ const SurveyList: React.FC = () => {
                 <ul>
                     <li>
                         <div className={Styles.surveyContent}>
+                            {/* <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC"></img> */}
+                            <div className={[Styles.showResponse, Styles.green].join(' ')}>
+                                👍
+                            </div>
                             <time>
                                 <span className={Styles.day}>12</span>
                                 <span className={Styles.month}>12</span>
