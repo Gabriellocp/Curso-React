c42797da0bbfe593f9475bd00acdf9fe52407f73
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-06 14:34:14 -0300

    feat: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -9,3 +9,6 @@ export const setCurrentAccountAdapter = (account: AccountModel): void => {
     makeLocalStorageAdapter().set('account', account)
 }
 
+export const getCurrentAccountAdapter = (): AccountModel => {
+    return makeLocalStorageAdapter().get('account')
+}
