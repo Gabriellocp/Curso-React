3572ee9d6171d6a96602467b3db13b20892025fc
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-09 13:45:59 -0300

    feat: ensure SurveyList shows 4 empty lists when loading page first time

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,29 +1,16 @@
-import { Footer, Header, ResponseIcon } from "@/presentation/components"
-import { IconName } from "@/presentation/components/response-icon/response-icon"
+import { Footer, Header } from "@/presentation/components"
 import React from "react"
+import { SurveyItemEmpty } from "./components"
 import Styles from './survey-list-styles.scss'
 
 const SurveyList: React.FC = () => {
     return (
-        <div className={Styles.surveyList}>
+        <div className={Styles.surveyList} >
             <Header></Header>
             <div className={Styles.mainContent}>
                 <h2> Enquetes </h2>
-                <ul>
-                    <li>
-                        <div className={Styles.surveyContent}>
-                            <ResponseIcon iconName={IconName.hasResponse}></ResponseIcon>
-                            <time>
-                                <span className={Styles.day}>12</span>
-                                <span className={Styles.month}>12</span>
-                                <hr></hr>
-                                <span className={Styles.year}>1202</span>
-                            </time>
-                            <p>Texto qualquer</p>
-                        </div>
-                        <footer> Ver resultado </footer>
-                    </li>
-                    <li></li>
+                <ul data-testid="surveyList">
+                    <SurveyItemEmpty></SurveyItemEmpty>
                 </ul>
 
             </div>
