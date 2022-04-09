6acdf2b03faa4031d45c642f87cbd2d12ee34b25
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-09 14:01:46 -0300

    feat: ensure SurveyList calls LoadSurveyList once when page is rendered

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,9 +1,19 @@
+import { LoadSurveyList } from "@/domain/usecases/load-survey-list"
 import { Footer, Header } from "@/presentation/components"
-import React from "react"
+import React, { useEffect } from "react"
 import { SurveyItemEmpty } from "./components"
 import Styles from './survey-list-styles.scss'
 
-const SurveyList: React.FC = () => {
+type Props = {
+    loadSurveyList: LoadSurveyList
+}
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+    useEffect(() => {
+        (async function () {
+            loadSurveyList.loadAll()
+        })()
+    }, [])
     return (
         <div className={Styles.surveyList} >
             <Header></Header>
