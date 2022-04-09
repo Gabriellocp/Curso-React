137696f6c4864268c7eba0479b9af625b52a1430
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-09 14:01:54 -0300

    test: ensure SurveyList calls LoadSurveyList once when page is rendered

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,19 +1,38 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import SurveyList from "./survey-list"
+import { LoadSurveyList } from "@/domain/usecases/load-survey-list"
+import { SurveyModel } from "@/domain/models"
 
 
 type SutTypes = {
-
+    loadSurveyListSpy: LoadSurveyListSpy
 }
 
-const makeSut = (): void => {
-    render(<SurveyList></SurveyList>)
+class LoadSurveyListSpy implements LoadSurveyList {
+    callsCount: number = 0
+    async loadAll(): Promise<SurveyModel[]> {
+        this.callsCount++
+        return []
+    }
+}
+
+const makeSut = (): SutTypes => {
+    const loadSurveyListSpy = new LoadSurveyListSpy()
+    render(<SurveyList loadSurveyList={loadSurveyListSpy}></SurveyList>)
+    return {
+        loadSurveyListSpy
+    }
 }
 describe('SurveyList component', () => {
     test('Should present 4 empty li\'s on load page', () => {
         makeSut()
         const surveyList = screen.getByTestId('surveyList')
         expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    })
+    test('Should call LoadSurveyList', () => {
+        const { loadSurveyListSpy } = makeSut()
+        expect(loadSurveyListSpy.callsCount).toBe(1)
+
     })
 })
