d33f69de80a767d1f8c3e5f579177b159e00d14b
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-13 15:28:03 -0300

    feat: ensure Header calls logout function with correct values

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,24 @@
-import React, { memo } from "react"
+import { ApiContext } from "@/presentation/contexts"
+import React, { memo, useContext } from "react"
+import { useHistory } from "react-router-dom"
 import Logo from "../logo/logo"
 import Styles from './header-styles.scss'
 
 const Header: React.FC = () => {
+    const { setCurrentAccount } = useContext(ApiContext)
+    const history = useHistory()
+    const logout: React.MouseEventHandler = (event: React.MouseEvent): void => {
+        event.preventDefault()
+        setCurrentAccount(undefined)
+        history.replace('/login')
+    }
     return (
         <header className={Styles.header}>
             <div className={Styles.headerContent}>
                 <Logo></Logo>
                 <div className={Styles.userInfo}>
                     <span> Gabriel </span>
-                    <a href="#"> Sair </a>
+                    <a data-testid='logout' href="#" onClick={logout} > Sair </a>
                 </div>
             </div>
         </header>
