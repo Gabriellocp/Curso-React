a10b1e7f87c2c48ec1ff964b74fb6ac649137c3f
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-13 13:07:19 -0300

    feat: ensure AuthorizeHttpGetClientDecorator return the same as HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -15,7 +15,6 @@ export class AuthorizeHttpClientDecorator implements HttpGetClient {
                 })
             })
         }
-        await this.httpGetClient.get(params)
-        return null
+        return await this.httpGetClient.get(params)
     }
 } 
