5d1c8dc2688013fb33994665322311d84b73a292
gabriellocp <gabriel_campista@hotmail.com>
2022-07-13 16:19:26 -0300

    test: ensure SurveyList present error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,22 @@
+import faker from 'faker'
+import * as Http from '../support/survey-list-mocks'
+import * as Helpers from '../support/helpers'
+const minPassLength: number = 5
+
+const simulateValidSubmit = (): void => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+}
+describe('SurveyList', () => {
+    beforeEach(() => {
+        Helpers.setLocalStorageItem('account',{accessToken:faker.random.uuid(), name: faker.name.findName()})
+        cy.visit('')
+    })
+    it('Should present error on UnexpectedError', () => {
+        Http.mockUnexpectedError()
+        //If some type of error occurs, put cy.visit('') here
+        cy.getByTestId('error').should('contain.text','Aconteceu algo inesperado...')
+    })
+   
+})
--- src/main/test/cypress/support/helpers.ts
+++ src/main/test/cypress/support/helpers.ts
@@ -11,5 +11,9 @@ export const testUrl = (pathUrl: string): void => {
 
 export const testLocalStorageItem = (key: string): void => {
     cy.window().then(window => assert.isOk(window.localStorage.getItem(key)))
+}
 
+
+export const setLocalStorageItem = (key: string, value: object): void => {
+    localStorage.setItem(key, JSON.stringify(value))
 }
