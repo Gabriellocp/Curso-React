5f3e46fe884eb6c996c81ab23e6fa7a636f19f99
gabriellocp <gabriel_campista@hotmail.com>
2022-07-25 16:29:15 -0300

    fix: change Spinner to not show console error

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -4,8 +4,8 @@ import Styles from './spinner-styles.scss'
 type Props = React.HTMLAttributes<HTMLElement> & {
     isNegative?: boolean
 }
-const Spinner: React.FC<Props> = (props: Props) => {
-    const negativeClass = props.isNegative ? Styles.negative : ''
+const Spinner: React.FC<Props> = ({isNegative,...props}: Props) => {
+    const negativeClass = isNegative ? Styles.negative : ''
     return (
         <div {...props} data-testid='spinner' className={[Styles.spinner, props.className, negativeClass].join(' ')}><div></div><div></div><div></div><div></div></div>
     )
