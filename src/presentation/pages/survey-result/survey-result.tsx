6b3e250a11031f4415aaff571034e09a19bc2811
gabriellocp <gabriel_campista@hotmail.com>
2022-07-26 17:29:12 -0300

    feat: ensure SurveyResult present error if LoadSurveyResult fails

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -3,6 +3,7 @@ import Styles from './survey-result-styles.scss'
 import FlipMove from "react-flip-move"
 import React, { useEffect, useState } from "react"
 import { LoadSurveyResult } from "@/domain/usecases"
+import { useErrorHandler } from "@/presentation/hooks"
 type Props = {
     loadSurveyResult: LoadSurveyResult
 }
@@ -17,9 +18,12 @@ const SurveyResult: React.FC<Props> = ({loadSurveyResult}:Props) =>{
     useEffect(()=>{
         loadSurveyResult.load()
         .then(surveyResult => setState(old => ({...old, surveyResult})))
-        .catch()
+        .catch(handleError)
 
     },[])
+    const handleError = useErrorHandler(
+        (error: Error) => setState(old=>({...old, surveyResult: null, error:error.message}))
+    )
     return (
         <div className={Styles.surveyResultWrap}>
            <Header></Header>
