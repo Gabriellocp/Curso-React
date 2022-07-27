c6448c8cfa76dbf59658bac0217654a9d8256dec
gabriellocp <gabriel_campista@hotmail.com>
2022-07-27 18:55:58 -0300

    test: ensure SurveyResult logout if statusCode returned is 403

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -3,6 +3,7 @@ import * as Helpers from '../utils/helpers'
 const path = /surveys/
 const mockUnexpectedError = (): void => Http.mockServerError(path)
 const mockSuccess = () : void => Http.mockOk(path, 'survey-result')
+const mockAccessDeniedError = ():void => Http.mockForbiddenError(path)
 describe('SurveyResult', () => {
     beforeEach(() => {
         cy.fixture('account').then(account=>{
@@ -21,6 +22,11 @@ describe('SurveyResult', () => {
         mockSuccess()
         cy.getByTestId('reload').click()
         cy.getByTestId('question').should('exist')
+    })
+    it('Should logout on AccessDeniedError', () => {
+        cy.visit('/surveys/any_id')
+        mockAccessDeniedError()
+        Helpers.testUrl('/login')
     })
     
 })
