bb00590f36d292cac37945acbfc117e4c63de637
gabriellocp <gabriel_campista@hotmail.com>
2022-07-27 19:05:24 -0300

    test: ensure SurveyResult present correct data on success

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -28,5 +28,25 @@ describe('SurveyResult', () => {
         mockAccessDeniedError()
         Helpers.testUrl('/login')
     })
+    it('Should present SurveyResult', () => {
+        cy.visit('/surveys/any_id')
+        mockSuccess()
+        cy.getByTestId('question').should('have.text', ' Question 1 ')
+        cy.getByTestId('day').should('have.text','03')
+        cy.getByTestId('month').should('have.text','fev')
+        cy.getByTestId('year').should('have.text','2018')
+        cy.get('li:nth-child(1)').then(li => {
+            assert.equal(li.find('[data-testid="answer"]').text(),'any_answer')
+            assert.equal(li.find('[data-testid="image"]').attr('src'),'any_image')
+            assert.equal(li.find('[data-testid="percent"]').text(),'70%')
+            }    
+        )
+        cy.get('li:nth-child(2)').then(li => {
+            assert.equal(li.find('[data-testid="answer"]').text(),'any_answer_2')
+            assert.notExists(li.find('[data-testid="image"]'))
+            assert.equal(li.find('[data-testid="percent"]').text(),'50%')
+            }    
+        )
+    })
     
 })
