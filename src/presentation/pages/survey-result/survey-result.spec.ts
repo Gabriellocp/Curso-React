be83c2cd94fa0527b1133cc38e8f9cfb338c9df0
gabriellocp <gabriel_campista@hotmail.com>
2022-07-30 15:51:09 -0300

    fix: changed test to prevent multiple answer clicks

--- src/presentation/pages/survey-result/survey-result.spec.ts
+++ src/presentation/pages/survey-result/survey-result.spec.ts
@@ -1,10 +1,12 @@
 import { fireEvent, screen, waitFor } from "@testing-library/react"
 import {SurveyResult} from "@/presentation/pages"
-import { LoadSurveyResultSpy, mockAccountModel, mockSurveyResultModel, SaveSurveyResultSpy } from "@/domain/test"
+import { LoadSurveyResultSpy, mockSurveyResultModel, SaveSurveyResultSpy } from "@/domain/test"
 import { AccessDeniedError, UnexpectedError } from "@/domain/errors"
 import {createMemoryHistory, MemoryHistory} from 'history'
 import { AccountModel } from "@/domain/models"
 import { renderWithHistory } from "@/presentation/test"
+import { LoadSurveyResult } from "@/domain/usecases"
+import { surveyRestultState } from "./components/atoms/atoms"
 
 type SutTypes = {
     loadSurveyResultSpy: LoadSurveyResultSpy
@@ -16,16 +18,25 @@ type SutTypes = {
 type SutParams = {
     loadSurveyResultSpy?: LoadSurveyResultSpy
     saveSurveyResultSpy?: SaveSurveyResultSpy
+    initialState?: 
+        {
+            isLoading: boolean,
+            error: string,
+            surveyResult: LoadSurveyResult.Model,
+            reload: boolean
+        }
+    
 }
 
-const makeSut = ({loadSurveyResultSpy = new LoadSurveyResultSpy(), saveSurveyResultSpy = new SaveSurveyResultSpy()} : SutParams = {}): SutTypes => {
+const makeSut = ({loadSurveyResultSpy = new LoadSurveyResultSpy(), saveSurveyResultSpy = new SaveSurveyResultSpy(), initialState = null} : SutParams = {}): SutTypes => {
     const history = createMemoryHistory({initialEntries: ['/', '/surveys/any_id'], initialIndex:1})
     const {setCurrentAccountMock} = renderWithHistory({
         history,
         Page: ()=> SurveyResult({
             loadSurveyResult: loadSurveyResultSpy,
             saveSurveyResult: saveSurveyResultSpy
-        })
+        }),
+        states: initialState ? [{atom: surveyRestultState, value: initialState}] : []
     })
 
     // render(
@@ -203,13 +214,17 @@ describe('SurveyResult Component', ()=> {
         
         })
     test('Should prevent multiple answer clicks',async ()=>{
-        const {saveSurveyResultSpy} = makeSut()
+        const initialState = {
+            isLoading: true,
+            error: '',
+            surveyResult: null,
+            reload: false
+        }
+        const {saveSurveyResultSpy} = makeSut({initialState})
         await waitFor(()=> screen.getByTestId('survey-result')) 
         const answersWrap = screen.queryAllByTestId('answer-wrap')
         fireEvent.click(answersWrap[1])
         await waitFor(()=> screen.getByTestId('survey-result'))
-        fireEvent.click(answersWrap[1])
-        await waitFor(()=> screen.getByTestId('survey-result'))
-        expect(saveSurveyResultSpy.callsCount).toBe(1)
+        expect(saveSurveyResultSpy.callsCount).toBe(0)
     })
 })
--- src/presentation/test/render-helper.tsx
+++ src/presentation/test/render-helper.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import {MemoryHistory} from 'history'
 import { Router } from 'react-router-dom'
-import { RecoilRoot } from 'recoil'
+import { MutableSnapshot, RecoilRoot, RecoilState } from 'recoil'
 import { mockAccountModel } from '@/domain/test'
 import { AccountModel } from '@/domain/models'
 import { currentAccountState } from '@/presentation/components'
@@ -11,21 +11,25 @@ type Params = {
     history: MemoryHistory
     Page: React.FC
     account?: AccountModel
+    states?: Array<{ atom: RecoilState<any>, value: any }>
 }
 
 type Result = {
     setCurrentAccountMock: (account: AccountModel)=> void
 }
 
-export const renderWithHistory = ({Page, history, account = mockAccountModel()}: Params): Result => {
+export const renderWithHistory = ({Page, history, account = mockAccountModel(), states = []}: Params): Result => {
     const setCurrentAccountMock = jest.fn()
     const mockedState = {
         setCurrentAccount: setCurrentAccountMock, 
         getCurrentAccount: ()=> account
     }
-
+    const initializeState = ({set}: MutableSnapshot):void =>{
+        [...states, { atom: currentAccountState, value: mockedState }].forEach(state=> set(state.atom,state.value))
+        
+    }
     render(
-        <RecoilRoot initializeState={({set})=> set(currentAccountState,mockedState)}>
+        <RecoilRoot initializeState={initializeState}>
             <Router history={history}>
                 <Page/>
             </Router>
